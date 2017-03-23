60b0d64c5c2c0d6f55c19c9858793e7ae48e8e3e
Vivek <vivek.nar87@gmail.com>
2017-03-23 19:40:31 +0530

    Added PATCH /todo/:id route

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ var env = process.env.NODE_ENV || 'development';
 if (env === 'development') {
     process.env.port = 3000;
     process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp'
-} else {
+} else if (env === 'test') {
     process.env.port = 3000;
     process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest'
 }
