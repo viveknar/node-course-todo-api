f622d54211d4a53dc8b43f12e23aaae36651dfbc
Vivek <vivek.nar87@gmail.com>
2017-03-30 16:50:24 +0530

    added password hashing

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 
 var UserSchema = new mongoose.Schema({
@@ -87,6 +88,23 @@ UserSchema.statics.findByToken = function (token) {
     });
 
 };
+
+UserSchema.pre('save', function (next) {
+    var user = this;
+
+    if(user.isModified('password')) {
+        // Hash and update password
+        var password = user.password;
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+})
 
 var User = mongoose.model('User', UserSchema);
 
